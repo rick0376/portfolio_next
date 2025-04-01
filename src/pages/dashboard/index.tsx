49258862b2a9e2823538ba88a5a162079b76a0ef
rick0376 <seu-email@dominio.com>
2025-04-01 17:51:26 -0300

    mobile / web

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,22 +4,44 @@ import Image from "next/image";
 import Link from "next/link";
 
 import fotoImg from "../../../public/assets/foto-pc.png";
-import projImg from "../../../public/assets/projeto1.png";
-import projImg1 from "../../../public/assets/projeto2.png";
-import projImg2 from "../../../public/assets/projeto3.png";
+import projImg from "../../../public/assets/dashboard/01_dashboard.png";
+import projImg1 from "../../../public/assets/dashboard/02_dashboard.png";
+import projImg2 from "../../../public/assets/dashboard/03_dashboard.png";
+import projImg3 from "../../../public/assets/dashboard/04_dashboard.png";
+import projImg4 from "../../../public/assets/projects/01_mobile.png";
+import projImg5 from "../../../public/assets/projects/02_mobile.png";
+import projImg6 from "../../../public/assets/projects/03_mobile.png";
+import projImg7 from "../../../public/assets/projects/01_frontEnd.png";
+import projImg8 from "../../../public/assets/projects/02_frontEnd.png";
+import projImg9 from "../../../public/assets/projects/03_frontEnd.png";
+import projImg10 from "../../../public/assets/projects/04_frontEnd.png";
 
-const projetos = [
-  { src: projImg, alt: "Projeto 1", link: "https://app.powerbi.com/view?r=eyJrIjoiOTA3NmYzZjItMWY1Zi00MzIwLThhOWYtNjAxOWM0MTQ0Njg5IiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
-  { src: projImg1, alt: "Projeto 2", link: "https://app.powerbi.com/view?r=eyJrIjoiMmY4MWM5M2ItMGZlMi00YmU5LTkwZTItN2Y3MDYxNmIwZjdhIiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
-  { src: projImg2, alt: "Projeto 3", link: "https://app.powerbi.com/view?r=eyJrIjoiODRkYmMwZTYtMDNjNS00NDUzLTg1YWMtNjlmMjgwNDUyMzVmIiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
-  
-  /*{ src: projImg, alt: "Projeto 1", link: "https://app.powerbi.com/view?r=eyJrIjoiOTA3NmYzZjItMWY1Zi00MzIwLThhOWYtNjAxOWM0MTQ0Njg5IiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },*/
+
+const projetosDashboard = [
+  { src: projImg, alt: "Dashboard 1", link: "https://app.powerbi.com/view?r=eyJrIjoiOTA3NmYzZjItMWY1Zi00MzIwLThhOWYtNjAxOWM0MTQ0Njg5IiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
+  { src: projImg1, alt: "Dashboard 2", link: "https://app.powerbi.com/view?r=eyJrIjoiMmY4MWM5M2ItMGZlMi00YmU5LTkwZTItN2Y3MDYxNmIwZjdhIiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
+  { src: projImg2, alt: "Dashboard 3", link: "https://app.powerbi.com/view?r=eyJrIjoiODRkYmMwZTYtMDNjNS00NDUzLTg1YWMtNjlmMjgwNDUyMzVmIiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
+  { src: projImg3, alt: "Dashboard 3", link: "https://app.powerbi.com/view?r=eyJrIjoiNjU5MjNiOTMtOTA2NC00ZmViLWIyMDItZWYwM2QxMWFiN2YyIiwidCI6IjJjYjkzN2JlLThjMjAtNGU3My1iNjMxLTdhOTA5OWY2ZWFhMyJ9" },
+];
+
+const projetosPrograms = [
+  { src: projImg4, alt: "Mobile 1", link: "/dashboard" },
+  { src: projImg5, alt: "Mobile 2", link: "/dashboard" },
+  { src: projImg6, alt: "Mobile 3", link: "/dashboard" },
+];
+
+const projetosProgramsWeb = [
+  { src: projImg7, alt: "FrontEnd 1", link: "/dashboard" },
+  { src: projImg8, alt: "FrontEnd 2", link: "/dashboard" },
+  { src: projImg9, alt: "FrontEnd 3", link: "/dashboard" },
+  { src: projImg10, alt: "FrontEnd 4", link: "/dashboard" },
+
 ];
 
 const imagens = [
-  "projeto1.png",
-  "projeto2.png",
-  "projeto3.png",
+  "01_dashboard.png",
+  "02_dashboard.png",
+  "03_dashboard.png",
   // Adicione mais imagens conforme necessário
 ];
 
@@ -69,7 +91,6 @@ export default function Dashboard() {
         <div>
           <h2>Sobre</h2>
           <p>
-            <br></br>
             Olá! <br></br>Sou Rick Pereira, formado em "Engenharia de Produção" com experiência como Inspetor da Qualidade.
             <br></br><br></br>
             Também fui Gerente Administrativo em posto de gasolina e Assistente Administrativo em Construtora Civil.
@@ -95,42 +116,67 @@ export default function Dashboard() {
       {/*INICIO PROJETOS*/}
       <section id="projetos" className={styles.projects}>
         <h2>Meus projetos</h2>
-        <p>Projetos Dashboard em Power BI</p>
-        <div>
-          {projetos.map((projeto, index) => (
-            <Link key={index} href={projeto.link} target="_blank" rel="noopener noreferrer">
-              <Image className={styles.projectsImg} src={projeto.src} alt={projeto.alt} />
-            </Link>
-          ))}
+        <div className={styles.divProjectMy}>
+          <p>Projetos Dashboard em Power BI</p>
+          <div>
+            {projetosDashboard.map((projetosDashboard, index) => (
+              <Link key={index} href={projetosDashboard.link} target="_blank" rel="noopener noreferrer">
+                <Image className={styles.projectsImg} src={projetosDashboard.src} alt={projetosDashboard.alt} />
+              </Link>
+            ))}
+          </div>
         </div>
 
-        <div className={styles.divLink}>
-          <div className={styles.divprojeto}>
-            <button className={styles.callbutonProject}>
-              <Link href="/" className={styles.link}>
-                <span className={styles.text}>Home</span>
+        <div className={styles.divProjectMy}>
+          <p>Projetos Mobile Comércio</p>
+          <div>
+            {projetosPrograms.map((projetosPrograms, index) => (
+              <Link key={index} href={projetosPrograms.link} target="_blank" rel="noopener noreferrer">
+                <Image className={styles.projectsImg2} src={projetosPrograms.src} alt={projetosPrograms.alt} />
               </Link>
-            </button>
+            ))}
           </div>
-
-          <div className={styles.divprojeto}>
-            <button className={styles.callbutonProject}>
-              <Link href="#sobre" className={styles.link}>
-                <span className={styles.text}>Sobre</span>
-              </Link>
-            </button>
-          </div>
-
-          <div className={styles.divprojeto}>
-            <button className={styles.callbutonProject}>
-              <Link href="/curriculo" className={styles.link}>
-                <span className={styles.text}>Meu Currículo</span>
-              </Link>
-            </button>
-          </div>
-
         </div>
+
+        <div className={styles.divProjectMy}>
+          <p>Projetos Web Comercio</p>
+          <div>
+            {projetosProgramsWeb.map((projetosProgramsWeb, index) => (
+              <Link key={index} href={projetosProgramsWeb.link} target="_blank" rel="noopener noreferrer">
+                <Image className={styles.projectsImg} src={projetosProgramsWeb.src} alt={projetosProgramsWeb.alt} />
+              </Link>
+            ))}
+          </div>
+        </div>
+
       </section>
+
+      <div className={styles.divLink}>
+        <div className={styles.divprojeto}>
+          <button className={styles.callbutonProject}>
+            <Link href="/" className={styles.link}>
+              <span className={styles.text}>Home</span>
+            </Link>
+          </button>
+        </div>
+
+        <div className={styles.divprojeto}>
+          <button className={styles.callbutonProject}>
+            <Link href="#sobre" className={styles.link}>
+              <span className={styles.text}>Sobre</span>
+            </Link>
+          </button>
+        </div>
+
+        <div className={styles.divprojeto}>
+          <button className={styles.callbutonProject}>
+            <Link href="/curriculo" className={styles.link}>
+              <span className={styles.text}>Currículo</span>
+            </Link>
+          </button>
+        </div>
+
+      </div>
       {/*-- FIM PROJETOS --*/}
 
       {/* INICIO RODAPE */}
